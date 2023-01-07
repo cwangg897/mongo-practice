fe8baf847d874a95f2d5f4c572558c50f238b48a
cwangg897 <dhkdrb897@gmail.com>
2023-01-07 13:10:40 +0900

    init

---
+++ src/main/java/com/example/notest/NotestApplication.java
@@ -0,0 +1,13 @@
+package com.example.notest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notest/NotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
